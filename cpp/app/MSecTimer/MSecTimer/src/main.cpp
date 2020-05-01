27f3444caad257be05f787ad6515de5c1aba555d
SulfredLee <sflee1112@gmail.com>
2020-05-01 13:38:41 +0800

    Add class case for MSecTimer

--- cpp/app/MSecTimer/MSecTimer/src/main.cpp
+++ cpp/app/MSecTimer/MSecTimer/src/main.cpp
@@ -4,11 +4,27 @@
 
 #include "MSecTimer.h"
 
+class FooClass
+{
+public:
+    FooClass() {}
+    ~FooClass() {}
+
+    static void PrintProxy(void *p, int x)
+    {
+        FooClass *c = (FooClass*)p;
+        c->Print(x);
+    }
+    void Print(int x)
+    {
+        std::cout << "Print from class " << x << std::endl;
+    }
+};
 void Foo(int x)
 {
-    std::cout << "Hello from " << __FUNCTION__ << " " << x << std::endl;
+    std::cout << "Print from otuside " << x << std::endl;
 }
-int main(int argc, char *argv[])
+void NormalCase()
 {
     MSecTimer timer(1000, Foo, 10);
     timer.Start();
@@ -18,6 +34,23 @@ int main(int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     timer.Stop();
+}
+void ClassCase()
+{
+    FooClass cc;
+    MSecTimer timer(1000, FooClass::PrintProxy, &cc, 10);
+    timer.Start();
+    int count = 0;
+    while (count++ < 5)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    timer.Stop();
+}
+int main(int argc, char *argv[])
+{
+    NormalCase();
+    ClassCase();
 
     return 0;
 }
