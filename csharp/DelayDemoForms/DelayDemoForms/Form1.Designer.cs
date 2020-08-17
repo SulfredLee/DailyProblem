fbeff17d8c7ce5b8e3310cf61894cd9670014a79
SulfredLee <sflee1112@gmail.com>
2020-08-17 09:06:11 +0800

    Adding new example for csharp DelayDemoForms

---
+++ csharp/DelayDemoForms/DelayDemoForms/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace DelayDemoForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numHours = new System.Windows.Forms.NumericUpDown();
+            this.numMins = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMins)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hour:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Minute:";
+            // 
+            // numHours
+            // 
+            this.numHours.Location = new System.Drawing.Point(50, 23);
+            this.numHours.Maximum = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            this.numHours.Name = "numHours";
+            this.numHours.Size = new System.Drawing.Size(71, 20);
+            this.numHours.TabIndex = 4;
+            this.numHours.Value = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            // 
+            // numMins
+            // 
+            this.numMins.Location = new System.Drawing.Point(50, 53);
+            this.numMins.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numMins.Name = "numMins";
+            this.numMins.Size = new System.Drawing.Size(71, 20);
+            this.numMins.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "use 24 hour clock.";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(510, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(542, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.DarkRed;
+            this.button3.Location = new System.Drawing.Point(510, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "STOP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Minute";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(48, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hour";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(285, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 148);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Run the code every:";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(111, 87);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(55, 17);
+            this.radioButton7.TabIndex = 17;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Month";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(112, 56);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(55, 17);
+            this.radioButton6.TabIndex = 16;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Month";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(112, 25);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(54, 17);
+            this.radioButton5.TabIndex = 15;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Week";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 120);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(44, 17);
+            this.radioButton4.TabIndex = 14;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Day";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 87);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(63, 17);
+            this.radioButton3.TabIndex = 13;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "HalfDay";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.numMins);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.numHours);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(24, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(255, 86);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Starting time";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Enabled = false;
+            this.progressBar1.Location = new System.Drawing.Point(510, 64);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(102, 23);
+            this.progressBar1.TabIndex = 15;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 166);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(600, 108);
+            this.listBox1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Scheduler log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 314);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Scheduler DEMO";
+            ((System.ComponentModel.ISupportInitialize)(this.numHours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMins)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numHours;
+        private System.Windows.Forms.NumericUpDown numMins;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ csharp/DelayDemoForms/DelayDemoForms/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelayDemoForms
+{
+    enum Scheduler
+    {
+        EveryMinutes,
+        EveryHour,
+        EveryHalfDay,
+        EveryDay,
+        EveryWeek,
+        EveryMonth,
+        EveryYear,
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numHours.Value = DateTime.Now.Hour;
+            numMins.Value = DateTime.Now.Minute;
+        }
+        CancellationTokenSource m_ctSource;
+
+        /// <summary>
+        /// Setting up time for running the code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+            //retrieve hour and minute from the form
+            int hour = (int)numHours.Value;
+            int minutes = (int)numMins.Value;
+
+            //create next date which we need in order to run the code
+            var dateNow = DateTime.Now;
+            var date = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, hour, minutes, 0);
+
+            listBox1.Items.Add("**********Scheduler has been started!*****");
+
+            //get nex date the code need to run
+            var nextDateValue=getNextDate(date,getScheduler());
+
+            runCodeAt(nextDateValue, getScheduler());
+
+        }
+
+      
+        /// <summary>
+        /// Schedule the time the need to be call
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="scheduler"></param>
+        private void runCodeAt(DateTime date,Scheduler scheduler )
+        {
+            m_ctSource = new CancellationTokenSource();
+
+            var dateNow = DateTime.Now;
+            TimeSpan ts;
+            if (date > dateNow)
+                ts = date - dateNow;
+            else
+            {
+                date = getNextDate(date, scheduler);
+                ts = date - dateNow;
+            }
+
+            //enable the progressbar
+            prepareControlForStart();
+
+           
+
+            //waits certan time and run the code, in meantime you can cancel the task at anty time
+            Task.Delay(ts).ContinueWith((x) => 
+                {
+                    //run the code at the time
+                     methodToCall(date);
+
+                     //setup call next day
+                     runCodeAt(getNextDate(date, scheduler), scheduler);
+
+                },m_ctSource.Token);
+        }
+
+        /// <summary>
+        /// prepare the controls for starting scheduler
+        /// </summary>
+        private void prepareControlForStart()
+        {
+            progressBar1.Enabled = true;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            button1.Enabled = false;
+            groupBox1.Enabled = false;
+            groupBox2.Enabled = false;
+            button3.Enabled = true;
+        }
+        /// <summary>
+        /// prepare the controls for canceling the scheduler
+        /// </summary>
+        private void prepareControlsForCancel()
+        {
+            m_ctSource = null;
+            progressBar1.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            groupBox1.Enabled = true;
+            groupBox2.Enabled = true;
+            button3.Enabled = false;
+            button1.Enabled = true;
+        }
+        /// <summary>
+        /// returns next date the code to be run
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="scheduler"></param>
+        /// <returns></returns>
+        private DateTime getNextDate(DateTime date, Scheduler scheduler)
+        {
+            switch (scheduler)
+            {
+                case Scheduler.EveryMinutes:
+                    return date.AddMinutes(1);
+                case Scheduler.EveryHour:
+                    return date.AddHours(1);
+                case Scheduler.EveryHalfDay:
+                    return date.AddHours(12);
+                case Scheduler.EveryDay:
+                    return date.AddDays(1);
+                case Scheduler.EveryWeek:
+                    return date.AddDays(7);
+                case Scheduler.EveryMonth:
+                    return date.AddMonths(1);
+                case Scheduler.EveryYear:
+                    return date.AddYears(1);
+                default:
+                    throw new Exception("Invalid scheduler");
+            }
+
+        }
+
+        /// <summary>
+        /// method to be called after period of time
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        private void methodToCall(DateTime time)
+        {
+            //setup next call
+            var nextTimeToCall = getNextDate(time,getScheduler());
+
+            this.BeginInvoke((Action)(() =>
+            {
+                var strText = string.Format("Method is called at {0}. The next call will be at {1}", time, nextTimeToCall);
+                listBox1.Items.Add(strText);
+                //MessageBox.Show(strText);
+            }));
+
+          
+           
+        }
+
+        /// <summary>
+        /// based on the selected radion box returns the scheduler enum
+        /// </summary>
+        /// <returns></returns>
+        private Scheduler getScheduler()
+        {
+            if(radioButton1.Checked)
+                return Scheduler.EveryMinutes;
+            if (radioButton2.Checked)
+                return Scheduler.EveryHour;
+            if (radioButton3.Checked)
+                return Scheduler.EveryHalfDay;
+            if (radioButton4.Checked)
+                return Scheduler.EveryDay;
+            if (radioButton5.Checked)
+                return Scheduler.EveryWeek;
+            if (radioButton6.Checked)
+                return Scheduler.EveryMonth;
+            if (radioButton7.Checked)
+                return Scheduler.EveryYear;
+
+            //default
+            return Scheduler.EveryMinutes;
+        }
+
+        /// <summary>
+        /// canceling the sheduler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            if (m_ctSource != null)
+            {
+                m_ctSource.Cancel();
+                prepareControlsForCancel();
+
+                listBox1.Items.Add("**********Scheduler has topped!*****");
+            }  
+        }
+
+      
+        /// <summary>
+        /// Exits the app
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Enabled)
+                MessageBox.Show("Scheduler is running. Cancel the scheduler first then close the application!");
+            else
+                this.Close();
+        }
+
+    }
+}
