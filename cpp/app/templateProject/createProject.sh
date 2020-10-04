ca1b2dc4c308ca75f63c8373f14495728c66d372
SulfredLee <sflee1112@gmail.com>
2020-10-04 11:51:08 +0800

    update template project supporting external project structure

--- cpp/app/templateProject/createProject.sh
+++ cpp/app/templateProject/createProject.sh
@@ -11,15 +11,32 @@ else
 fi
 srcProject="templateProject"
 
-mkdir -p $projectName/$projectName/src
-mkdir -p $projectName/$projectName/lib/$projectName
+mkdir -p $projectName/Projects/$projectName
+mkdir -p $projectName/Projects/$projectName/src
+mkdir -p $projectName/Projects/$projectName/lib/$projectName
+mkdir -p $projectName/debug
+mkdir -p $projectName/release
+mkdir -p $projectName/install
 
+# handle root external cmake
+cp $srcProject/Projects/CMakeLists.txt $projectName/Projects
+sed -i "s/$srcProject/$projectName/g" "$projectName/Projects/CMakeLists.txt"
+cp $srcProject/Projects/$srcProject.cmake $projectName/Projects/$projectName.cmake
+sed -i "s/$srcProject/$projectName/g" "$projectName/Projects/$projectName.cmake"
+
+# handle sub project
+srcSubProjectRoot="$srcProject/Projects/$srcProject"
+targetSubProjectRoot="$projectName/Projects/$projectName"
 # handle root CMakeLists.txt
-cp ./$srcProject/CMakeLists.txt $projectName/$projectName
-sed -i "s/$srcProject/$projectName/g" "$projectName/$projectName/CMakeLists.txt"
+cp $srcSubProjectRoot/CMakeLists.txt $targetSubProjectRoot
+sed -i "s/$srcProject/$projectName/g" "$targetSubProjectRoot/CMakeLists.txt"
 # handle lib CMakeLists.txt
-cp ./$srcProject/lib/CMakeLists.txt $projectName/$projectName/lib
-cp ./$srcProject/lib/$srcProject/CMakeLists.txt $projectName/$projectName/lib/$projectName
+cp $srcSubProjectRoot/lib/CMakeLists.txt $targetSubProjectRoot/lib
+cp $srcSubProjectRoot/lib/$srcProject/CMakeLists.txt $targetSubProjectRoot/lib/$projectName
 # handle src CMakeLists.txt
-cp ./$srcProject/src/CMakeLists.txt $projectName/$projectName/src
-sed -i "s/$srcProject/$projectName/g" "$projectName/$projectName/src/CMakeLists.txt"
+cp $srcSubProjectRoot/src/CMakeLists.txt $targetSubProjectRoot/src
+sed -i "s/$srcProject/$projectName/g" "$targetSubProjectRoot/src/CMakeLists.txt"
+
+# copy ProjectB as an example
+cp -rf $srcProject/Projects/ProjectB $projectName/Projects
+cp $srcProject/Projects/ProjectB.cmake $projectName/Projects
