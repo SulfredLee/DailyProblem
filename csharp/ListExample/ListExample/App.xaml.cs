2be9825b3ff60580c52f0048a6053284f65f0d02
SulfredLee <sflee1112@gmail.com>
2020-11-29 07:37:47 +0800

    Add List example

---
+++ csharp/ListExample/ListExample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ListExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register container view types here
+            ViewModelLocationProvider.Register<MainWindow, MainWindowViewModel>();
+        }
+        protected override Window CreateShell()
+        {
+            return null;
+        }
+    }
+}
---
+++ csharp/ListExample/ListExample/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public class ServiceNode : BindableBase
+        {
+            private string _serviceName;
+            public string ServiceName
+            {
+                get => _serviceName;
+                set => SetProperty(ref _serviceName, value);
+            }
+            private string _instanceName;
+            public string InstanceName
+            {
+                get => _instanceName;
+                set => SetProperty(ref _instanceName, value);
+            }
+            private string _state;
+            public string State
+            {
+                get => _state;
+                set => SetProperty(ref _state, value);
+            }
+        }
+        public ObservableCollection<ServiceNode> ServiceObjects { get; } = new ObservableCollection<ServiceNode>();
+        public MainWindowViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ServiceObjects.Add(new ServiceNode { InstanceName = "apple", ServiceName = "AP", State = "Normal" });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                ServiceObjects.Add(new ServiceNode { InstanceName = "orange", ServiceName = "OR", State = "Degraded" });
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                ServiceObjects.Add(new ServiceNode { InstanceName = "banana", ServiceName = "BA", State = "Down" });
+            }
+        }
+    }
+}
