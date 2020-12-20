ca39130089f2c5bde6a039b2fe93e2fadb45d672
SulfredLee <sflee1112@gmail.com>
2020-12-20 12:06:31 +0800

    Add Prism example PrismOutlook, lesson 1
    Installed Infragistic
    Added different modules
    Installed Prism template pack from brian lagunas

---
+++ csharp/PrismOutlook/Modules/PrismOutlook.Modules.Contacts/Menus/ContactsGroup.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Infragistics.Windows.OutlookBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrismOutlook.Modules.Contacts.Menus
+{
+    /// <summary>
+    /// Interaction logic for ContactsGroup.xaml
+    /// </summary>
+    public partial class ContactsGroup : OutlookBarGroup
+    {
+        public ContactsGroup()
+        {
+            InitializeComponent();
+        }
+    }
+}
