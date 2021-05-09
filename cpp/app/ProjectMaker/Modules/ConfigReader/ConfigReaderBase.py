c032a2113471254a8ff0597ef46df1caf3579f59
SulfredLee <sflee1112@gmail.com>
2021-05-09 09:20:17 +0800

    remove old projects

--- cpp/app/ProjectMaker/Modules/ConfigReader/ConfigReaderBase.py
+++
@@ -1,9 +0,0 @@
-from Business.ConfigData import *
-
-class ConfigReaderBase(object):
-    def __init__(self):
-        pass
-
-    # pure virtual
-    def ReadFrom(self, srcName: str):
-        raise NotImplementedError
