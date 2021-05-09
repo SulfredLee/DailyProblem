7cddf4b82e4b62c769bbc62a9114353812f8ffeb
SulfredLee <sflee1112@gmail.com>
2021-05-09 12:46:33 +0800

    added cpp lib and qt example

---
+++ cpp/app/SFLib/Projects/SFLib/app/SimpleMain/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <QApplication>
+#include "mainwindow.h"
+
+int main (int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    MainWindow MW;
+    MW.show();
+    bool bRTN = app.exec();
+
+    return bRTN;
+}
