07f540c543e66a5f92f2ddf1f22255fddd5fdc29
SulfredLee <sflee1112@gmail.com>
2021-06-27 20:56:46 +0800

    Add QuickFixDataCtrl

---
+++ cpp/app/TestPlayGround/lib/QuickFixDataCtrl/test/TestQuickFixDataCtrl.cpp
@@ -0,0 +1,16 @@
+#include "TestQuickFixDataCtrl.h"
+
+/*
+EXPECT_EQ(val1,val2);, val1 == val2
+EXPECT_NE(val1,val2);, val1 != val2
+EXPECT_LT(val1,val2);, val1 < val2
+EXPECT_LE(val1,val2);, val1 <= val2
+EXPECT_GT(val1,val2);, val1 > val2
+EXPECT_GE(val1,val2);, val1 >= val2
+*/
+
+TEST_F(TestQuickFixDataCtrl, Test001)
+{
+    EXPECT_EQ(true, true);
+}
+
---
+++ cpp/app/TestPlayGround/lib/QuickFixDataCtrl/test/TestQuickFixDataCtrl.h
@@ -0,0 +1,14 @@
+#ifndef TEST_QuickFixDataCtrl_H
+#define TEST_QuickFixDataCtrl_H
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class TestQuickFixDataCtrl : public testing::Test
+{
+ public:
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
+
+#endif
+
