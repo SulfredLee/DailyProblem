46d844827643bc63985ef7e5542a4ecc88ddb7f6
SulfredLee <sflee1112@gmail.com>
2021-06-27 13:40:22 +0800

    update gtest use case

--- cpp/app/CCreater/CCreate.sh
+++ cpp/app/CCreater/CCreate.sh
@@ -325,6 +325,15 @@ class Test${projectName} : public testing::Test
 
     echo "#include \"Test${projectName}.h\"
 
+/*
+EXPECT_EQ(val1,val2);, val1 == val2
+EXPECT_NE(val1,val2);, val1 != val2
+EXPECT_LT(val1,val2);, val1 < val2
+EXPECT_LE(val1,val2);, val1 <= val2
+EXPECT_GT(val1,val2);, val1 > val2
+EXPECT_GE(val1,val2);, val1 >= val2
+*/
+
 TEST_F(Test${projectName}, Test001)
 {
     EXPECT_EQ(true, true);
