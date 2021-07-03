126f071c5cf4ebe90afd7586f2f559474647ff4a
SulfredLee <sflee1112@gmail.com>
2021-07-03 10:10:31 +0800

    Add json example

---
+++ csharp/JSonExample/JSonExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSonExample
+{
+    class Program
+    {
+        public class EligibleRole
+        {
+            public List<string> theArray { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string json = System.IO.File.ReadAllText("ConfigFile.json");
+            var eligible_role = JsonConvert.DeserializeObject<EligibleRole>(json);
+            foreach (var ele in eligible_role.theArray)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
