3afc6920ea599f433f8cf829b100536ae557f2e2
SulfredLee <sflee1112@gmail.com>
2022-12-05 17:41:56 +0800

    Update template_start_dev_container_sh.py
    update

--- devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/dev/template_start_dev_container_sh.py
+++ devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/dev/template_start_dev_container_sh.py
@@ -2,6 +2,7 @@ content_st = """
 #!/bin/bash
 
 DOCKER_VERSION=$(grep "DOCKER_BUILDER_VERSION:" ../../.gitlab-ci.yml | cut -c 27-)
+rm -rf ${PWD}/../../virtualenvs/ || true
 mkdir -p ${PWD}/../../virtualenvs/
 docker run -it -v "${PWD}/../../:/python/project:rw"\
                -v "${PWD}/../../virtualenvs/:/home/$(whoami)/.cache/pypoetry/virtualenvs_mount/"\
