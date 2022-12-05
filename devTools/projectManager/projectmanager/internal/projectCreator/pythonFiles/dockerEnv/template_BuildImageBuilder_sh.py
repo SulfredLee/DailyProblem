a97dc37b7877d5709afa51db23349b23a2438f7a
SulfredLee <sflee1112@gmail.com>
2022-12-05 14:30:13 +0800

    Update template_BuildImageBuilder_sh.py
    udpate

--- devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/template_BuildImageBuilder_sh.py
+++ devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/template_BuildImageBuilder_sh.py
@@ -2,7 +2,10 @@ content_st = """
 #!/bin/bash
 
 DOCKER_VERSION=$(grep "DOCKER_BUILDER_VERSION:" ../.gitlab-ci.yml | cut -c 27-)
-docker build --build-arg DOCKER_UID=$(whoami) --build-arg DOCKER_GID=$(whoami) --target builder -t {{ project_name }}:${DOCKER_VERSION}_$(whoami) ..
+docker build --build-arg DOCKER_UNAME=$(whoami)\
+             --build-arg DOCKER_GNAME=$(whoami)\
+             --build-arg DOCKER_UID=$(id -u)\
+             --build-arg DOCKER_GID=$(id -g) --target builder -t flask_example_001:${DOCKER_VERSION}_$(whoami) ..
 
 echo "$ cd ./dev"
 # echo "$ docker-compose run {{ project_name }} bash" # old way
