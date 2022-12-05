140df3ff110866ee2eb8c2c2a59f830c4c6eeb3a
SulfredLee <sflee1112@gmail.com>
2022-12-05 14:28:40 +0800

    update python dev container
    update

--- devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/dev/template_start_dev_container_sh.py
+++ devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/dockerEnv/dev/template_start_dev_container_sh.py
@@ -5,5 +5,5 @@ DOCKER_VERSION=$(grep "DOCKER_BUILDER_VERSION:" ../../.gitlab-ci.yml | cut -c 27
 mkdir -p ${PWD}/../../virtualenvs/
 docker run -it -v "${PWD}/../../:/python/project:rw"\
                -v "${PWD}/../../virtualenvs/:/home/$(whoami)/.cache/pypoetry/virtualenvs_mount/"\
-               --env-file ./.env -u $(id -u):$(id -g) {{ project_name }}:${DOCKER_VERSION}_$(whoami) bash -c "cp -rf ~/.cache/pypoetry/virtualenvs/* ~/.cache/pypoetry/virtualenvs_mount/; bash"
+               --env-file ./.env -u $(id -u):$(id -g) {{ project_name }}:${DOCKER_VERSION}_$(whoami) bash -c "cp -rf /home/$(whoami)/.cache/pypoetry/virtualenvs/* /home/$(whoami)/.cache/pypoetry/virtualenvs_mount/; bash"
 """
