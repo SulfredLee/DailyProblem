c3b2a0e86c57e2d2c741dd0cfcaa3bbd9918a8b4
SulfredLee <sflee1112@gmail.com>
2023-01-06 10:08:09 +0800

    [Feature][Compress Movie] No need to check movie size after added quality checking

--- devTools/convertMovie_h265.sh
+++ devTools/convertMovie_h265.sh
@@ -77,7 +77,7 @@ elif [ "$ACTION" = "check_status" ]; then
 elif [ "$ACTION" = "gpu_usage" ]; then
     nvidia-smi -l 2
 elif [ "$ACTION" = "convert_recursive" ]; then
-    readarray -d '' movie_list < <(find "$initialPath" -type f \( -name "*.mp4" -o -name "*.wmv" -o -name "*.mkv" \) -size +1G -print0)
+    readarray -d '' movie_list < <(find "$initialPath" -type f \( -name "*.mp4" -o -name "*.wmv" -o -name "*.mkv" \) -print0)
     ERROR_LOG_FILE="${initialPath}/convert_failed.log"
     for i in "${!movie_list[@]}"; do
         echo "conversion progress: $(( i + 1 ))/${#movie_list[@]}"
