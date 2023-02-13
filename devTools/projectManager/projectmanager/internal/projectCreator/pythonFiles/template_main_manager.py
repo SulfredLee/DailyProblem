09663ce39da3cec5576165ce617631e56f2ba031
SulfredLee <sflee1112@gmail.com>
2023-02-13 14:39:13 +0800

    [Feature] Add resource clean up in main_manager

--- devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/template_main_manager.py
+++ devTools/projectManager/projectmanager/internal/projectCreator/pythonFiles/template_main_manager.py
@@ -4,14 +4,20 @@ from typing import List, Dict, Tuple
 
 import sfdevtools.observability.log_helper as lh
 from sfdevtools.devTools.SingletonDoubleChecked import SDC
+import atexit
 
 class MainManager(SDC):
     def __init__(self):
         self.__logger: logging.Logger = None
+
+        atexit.register(self.__cleanup)
 
     def init_component(self, logger: logging.Logger):
         self.__logger = logger
 
     def get_logger(self) -> logging.Logger:
         return self.__logger
+
+    def __cleanup(self):
+        pass
 """
