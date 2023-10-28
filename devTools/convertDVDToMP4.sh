abad30733acb80e1d731997e580a83fe2d543c12
SulfredLee <sflee1112@gmail.com>
2023-10-28 17:58:51 +0800

    [Patch] update the dvd conversion script

--- devTools/convertDVDToMP4.sh
+++ devTools/convertDVDToMP4.sh
@@ -6,12 +6,13 @@ echo "user call script from: ${initialPath}"
 
 function PrintHelp {
     echo "Usage:"
-    echo "\$ $0 --dvd_path <DVD path>"
+    echo "\$ $0 (--dvd_path|-p) <DVD path> (--dry_run|-d)"
 }
 
 
 POSITIONAL=()
 DVD_PATH=""
+DRY_RUN=""
 CURRENT_DATE=`date +%Y%m%d`
 #can specify a date(YYYYMMDD) for which the script will re-generate (for T-1)
 
@@ -25,10 +26,14 @@ do
             PrintHelp
             shift # past argument
             ;;
-        -d|--dvd_path)
+        -p|--dvd_path)
             DVD_PATH="$2"
             shift # past argument
             shift # past value
+            ;;
+        -d|--dry_run)
+            DRY_RUN="true"
+            shift # past argument
             ;;
         *)    # unknown option
             POSITIONAL+=("$1") # save it in an array for later
@@ -42,30 +47,27 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [ "$DVD_PATH" != "" ]; then
     echo "copy VOB files from: $DVD_PATH"
-    if [ -f "$DVD_PATH/VTS_01_7.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_7.VOB"
-        cp "$DVD_PATH/VTS_01_7.VOB" "$DVD_PATH/VTS_01_6.VOB" "$DVD_PATH/VTS_01_5.VOB" "$DVD_PATH/VTS_01_4.VOB" "$DVD_PATH/VTS_01_3.VOB" "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_6.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_6.VOB"
-        cp "$DVD_PATH/VTS_01_6.VOB" "$DVD_PATH/VTS_01_5.VOB" "$DVD_PATH/VTS_01_4.VOB" "$DVD_PATH/VTS_01_3.VOB" "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_5.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_5.VOB"
-        cp "$DVD_PATH/VTS_01_5.VOB" "$DVD_PATH/VTS_01_4.VOB" "$DVD_PATH/VTS_01_3.VOB" "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_4.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_4.VOB"
-        cp "$DVD_PATH/VTS_01_4.VOB" "$DVD_PATH/VTS_01_3.VOB" "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_3.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_3.VOB"
-        cp "$DVD_PATH/VTS_01_3.VOB" "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_2.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_2.VOB"
-        cp "$DVD_PATH/VTS_01_2.VOB" "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
-    elif [ -f "$DVD_PATH/VTS_01_1.VOB" ]; then
-        echo "Found $DVD_PATH/VTS_01_1.VOB"
-        cp "$DVD_PATH/VTS_01_1.VOB" "ConCat.vob"
+
+    # get the list of files
+    # sometime the VTS_01_01.VOB is the declaration of FBI warning, you may want to skip it manually
+    readarray -d '' movie_list < <(find "$DVD_PATH" -type f \( -name "VTS_01_*.VOB" \) -print0)
+    FILE_LIST=""
+    for i in "${!movie_list[@]}"; do
+        echo "Get file: ${movie_list[$i]}"
+        FILE_LIST="${FILE_LIST} ${movie_list[$i]}"
+    done
+    CMD_LINE="cat ${FILE_LIST} > ConCat.vob"
+    echo "${CMD_LINE}"
+    if [ "$DRY_RUN" != "true" ]; then
+        eval "$CMD_LINE"
     fi
 
-    ffmpeg -loglevel warning -i "ConCat.vob" -codec:a copy -codec:v hevc_nvenc "converted.mp4"
+    echo "ffmpeg -i \"ConCat.vob\" -c:a copy -c:s copy -c:v hevc_nvenc \"converted.mp4\""
+    if [ "$DRY_RUN" != "true" ]; then
+        ffmpeg -i "ConCat.vob" -c:a copy -c:v hevc_nvenc "converted.mp4"
+        # you can use map to select the channel you want
+        # https://trac.ffmpeg.org/wiki/Map
+    fi
 else
     echo "Action : ${ACTION} not recognized"
     PrintHelp
