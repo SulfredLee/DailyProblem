c4b32ddb6d7a19c6fd71cf6dc79343583d49b1ac
SulfredLee <sflee1112@gmail.com>
2024-12-09 19:06:20 +0100

    [Patch] add cpp standard libraries headers to gtags prepare script

--- devTools/prepareSpacemacsTags_selection.sh
+++ devTools/prepareSpacemacsTags_selection.sh
@@ -4,6 +4,14 @@ buildPath="$PWD"
 userRoot="/home/userName"
 fwRoot="${userRoot}/your_framework_libraries"
 
+## run the follow commands to see where does cpp compiler read header files?
+# `gcc -print-prog-name=cc1plus` -v
+# `gcc -print-prog-name=cpp` -v
+## then you can copy the file to the gtags root path, usually that is /home/user
+# rsync -zarv  --prune-empty-dirs --include "*/"  --include="*.h" --include="*.cpp" --include="*.c" --include="*.hpp" --exclude="*" "from" "to"
+cppRoot=""
+
+
 cd ${userRoot}
 
 # remove old tags
@@ -23,7 +31,8 @@ rm ${userRoot}/cscope.po.out
 
 # make tags
 echo "######### make GTag"
-find -L ${fwRoot}/ ${buildPath} -type f -print > gtags.files
+find -L ${fwRoot}/ -name "*.cc" -o -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" > gtags.files
+find -L ${buildPath}/ -name "*.cc" -o -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" >> gtags.files
 gtags
 
 echo "######### make cscope"
